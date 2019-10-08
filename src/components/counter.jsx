74dc095da332f568603c718daf5c69de97bb5b9a
steven-johnstone <steven.johnstone@ada.ac.uk>
2019-10-08 15:29:21 +0100

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { link } from "fs";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value
+  };
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>Counter#{this.props.id}</h4>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
